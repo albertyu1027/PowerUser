466c9ecfeb072e81777d2d3b95e23dd7e89327ca
Albert Yu <albertyu@alberts-air.attlocal.net>
2018-01-18 09:10:29 -0800

    added Compare with Friends Page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,6 +14,15 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// // Set up promises with mongoose
+// mongoose.Promise = global.Promise;
+
+// //connect to mongo db
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/27017",
+
+// );
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
