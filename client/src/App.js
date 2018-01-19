dae3912498ddc403904c80f04ed098722fb6f577
Albert Yu <albertyu@Alberts-Air.att.net>
2018-01-19 01:19:35 -0800

    linked all three pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import './App.css';
 import Chart from './pages/FrdChart';
+import Dashboard from './pages/Dashboard';
+import Upload from './pages/Upload';
+import Nav from "./components/Nav";
 
-class App extends Component {
-  render() {
-    return (
+const App = () =>
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/dash" component={Dashboard} />
+        <Route exact path="/upload" component={Upload} />
+        <Route exact path="/frd" component={Chart} />
+      </Switch>
+    </div>
+  </Router>;
+
       <div className="App">
         <Chart />
       </div>
-    );
-  }
-}
 
 export default App;
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Nav = () =>
+<nav className="navbar navbar-inverse navbar-top">
+  <div className="container-fluid">
+    <div className="navbar-header">
+
+      <a href="/dash" className="navbar-brand">
+        Home
+      </a>
+
+      <a href="/upload" className="navbar-brand">
+        Upload
+      </a>
+
+      <a href="/frd" className="navbar-brand">
+        Compete
+      </a>
+    </div>
+  </div>
+</nav>;
+
+export default Nav;
