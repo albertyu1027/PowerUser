f6d02ac2ea7e4e923f6e44f340d42724de85caa8
adham bishr <adhambishr@gmail.com>
2018-01-19 13:39:26 -0800

    added basic login layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import './App.css';
-import Chart from './pages/FrdChart';
-import Dashboard from './pages/Dashboard';
-import Upload from './pages/Upload';
+import "./App.css";
+import Chart from "./pages/FrdChart";
+import Dashboard from "./pages/Dashboard";
+import Upload from "./pages/Upload";
 import Nav from "./components/Nav";
+import LoginForm from "./pages/LoginForm";
+import SignupForm from "./pages/SignupForm";
 
-const App = () =>
+const App = () => (
   <Router>
     <div>
       <Nav />
@@ -17,12 +19,15 @@ const App = () =>
         <Route exact path="/dash" component={Dashboard} />
         <Route exact path="/upload" component={Upload} />
         <Route exact path="/frd" component={Chart} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/signup" component={SignupForm} />
       </Switch>
     </div>
-  </Router>;
+  </Router>
+);
 
-      <div className="App">
-        <Chart />
-      </div>
+<div className="App">
+  <Chart />
+</div>;
 
 export default App;
