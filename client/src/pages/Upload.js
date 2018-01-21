43765ade2b932740bbc14105b5091bb278d69104
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-21 15:30:09 -0800

    Uppy pkg and upload route

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
 // import {Bar} from 'react-chartjs-2';
 import { Link } from "react-router-dom";
+import Uppy from 'uppy/lib/core'
+import Dashboard from 'uppy/lib/plugins/Dashboard'
+import Tus from 'uppy/lib/plugins/Tus'
+ 
+Uppy({ autoProceed: false })
+  .use(Dashboard, { trigger: '#select-files' })
+  .use(Tus, { endpoint: 'https://master.tus.io/files/' })
+  .run()
+  .on('complete', (result) => {
+    console.log('Upload result:', result)
+  })
 
 class Upload extends Component {
 
@@ -8,6 +19,7 @@ class Upload extends Component {
       return (
         <div className="chart">
             <h1> Upload Page! </h1>
+            <div id="select-files"></div>
         </div>
         )
       }
--- server.js
+++ server.js
@@ -1,43 +1,24 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
+// const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
+const uploadRoutes = require("./routes/upload");
+const app = express();
 
-// Configure body parser for AJAX requests
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// Serve up static assets
-app.use(express.static("client/build"));
-// Add routes, both API and view
-app.use(routes);
-
-
-// Set up promises with mongoose
-mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
-  {
-    useMongoClient: true
-  }
-);
+// Upload routes
+app.use("/upload", uploadRoutes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
+}
 
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-}
-
-
 
 // // Set up promises with mongoose
 // mongoose.Promise = global.Promise;
