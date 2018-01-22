6c6cc0218cd0a9627fe98b51df9a0e9bec445c7d
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-21 20:02:09 -0800

    provide wildcard route

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use(routes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  // const path = require("path");
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/poweruser";
