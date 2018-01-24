5b43d8e52b1ac2060f23b04c694c4f7fcbb64798
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-24 00:22:28 -0800

    prod vs dev

--- routes/upload.js
+++ routes/upload.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-var storage = multer.memoryStorage();
-var upload = multer({ storage: storage });
 
-router.post("/", upload.single("pgeCsv"), function (req, res, next) {
+if (process.env.NODE_ENV === "production") {
+	var storage = multer.memoryStorage();
+	var upload = multer({
+		storage: storage
+	});
+} else {
+	var upload = multer({
+		dest: "uploads/"
+	});
+}
+
+router.post("/", upload.single("pgeCsv"), function (req, res) {
 	// req.body will hold the text fields, if there were any
 	// req.file is the `pgeData` file
-	//console.log(req);
-	console.log(req.file.buffer);
-	res.json(req.file);
-  });
+
+	if (process.env.NODE_ENV === "production") {
+		let file = req.file.buffer.toString();
+		res.send(file);
+	} else {
+		console.log(req.file);
+		res.json(req.file);
+	}
+});
 
 module.exports = router;
