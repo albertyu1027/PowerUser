c2ef43111ea7c3db717dc4025a96ce239a7610e6
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-23 18:11:27 -0800

    send json response

--- routes/upload.js
+++ routes/upload.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 var upload = multer({
-	dest: "../tmp"
+	dest: "/tmp"
 });
 
 router.post("/", upload.single("pgeCsv"), function (req, res, next) {
 	// req.body will hold the text fields, if there were any
 	// req.file is the `pgeData` file
-	console.log(req);
+	//console.log(req);
+	res.json(req.file);
   });
 
 module.exports = router;
