2b0c0421150e1fdcc25c939e4e7d7bd41141b69f
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-24 12:21:44 -0800

    Parse csv data locally 👌🏼

--- routes/upload.js
+++ routes/upload.js
@@ -1,40 +1,39 @@
-// Require file-system module. This will be used to read the csv received from the user
-const fs = require("fs");
+// Require csvtojson pkg to convert csv data to JSON
+const csv = require('csvtojson');
 // Require Express.js to setup the routes
 const express = require("express");
 const router = express.Router();
 // Require the Multer pkg which will help us receive and read the file contents
 const multer = require("multer");
 
-// Setup Multer's storage options for development and production environments
-if (process.env.NODE_ENV === "production") {
-	// We use memory storage in production because Heroku's free dyno's do not offer simple file storage
-	var storage = multer.memoryStorage();
-	var upload = multer({
-		storage: storage
-	});
-} else {
-	// If running locally, Multer will save to 'uploads' directory on the developer's computer
-	var upload = multer({
-		dest: "uploads/"
-	});
-}
+// Use Multer's memory storage because Heroku's free dyno's do not offer simple file storage
+var storage = multer.memoryStorage();
+var upload = multer({
+	storage: storage
+});
 
 router.post("/", upload.single("pgeCsv"), function (req, res) {
 
 	/* ============================== TESTING FILE RESPONSE ============================== */
 	// req.body will hold the text fields, if there were any
 	// req.file is the `pgeData` file
-	if (process.env.NODE_ENV === "production") {
-		let file = req.file.buffer.toString();
-		res.send(file);
-	} else {
-		console.log(req.file);
-		res.json(req.file);
-	}
+	let file = req.file.buffer.toString().trim();
+	console.log(file);
+	let splitCSV = "TYPE," + file.split("TYPE,")[1];
+
+	csv({
+			noheader: false
+		})
+		.fromString(splitCSV)
+		.on('csv', (csvRow) => { //this func will be called twice. Header row will not be populated
+			console.log("CSV Row:", csvRow);
+		})
+		.on('done', () => {
+			res.send(splitCSV);
+		});
 	/* ============================== TESTING FILE RESPONSE ============================== */
 
-	
+
 
 });
 
