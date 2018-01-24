7dda752fa5d51e4ea758fbd4baf17655dea486f4
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-24 00:08:31 -0800

    use memoryStorage

--- routes/upload.js
+++ routes/upload.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-var upload = multer({
-	dest: "uploads/"
-});
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 router.post("/", upload.single("pgeCsv"), function (req, res, next) {
 	// req.body will hold the text fields, if there were any
 	// req.file is the `pgeData` file
 	//console.log(req);
+	console.log(req.file.buffer);
 	res.json(req.file);
   });
 
