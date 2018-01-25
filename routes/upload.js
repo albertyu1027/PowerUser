1c8f941e6d71e5d7a829ddcdcc69f0c427d36335
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-24 19:14:59 -0800

    Update Upload schema. Insert mock data to MongoDB.

--- routes/upload.js
+++ routes/upload.js
@@ -3,6 +3,7 @@ const csv = require('csvtojson');
 // Require Express.js to setup the routes
 const express = require("express");
 const router = express.Router();
+const UploadData = require("../models/upload");
 // Require the Multer pkg which will help us receive and read the file contents
 const multer = require("multer");
 
@@ -18,22 +19,31 @@ router.post("/", upload.single("pgeCsv"), function (req, res) {
 	// req.body will hold the text fields, if there were any
 	// req.file is the `pgeData` file
 	let file = req.file.buffer.toString().trim();
-	console.log(file);
 	let splitCSV = "TYPE," + file.split("TYPE,")[1];
+
+	var uploadObject = {
+		id:"1111",
+		date: "11/28/2017",
+		kwhUsage: 18.06,
+		cost: 3.63
+	};
 
 	csv({
 			noheader: false
 		})
 		.fromString(splitCSV)
-		.on('csv', (csvRow) => { //this func will be called twice. Header row will not be populated
+		.on('csv', (csvRow) => {
 			console.log("CSV Row:", csvRow);
+
 		})
 		.on('done', () => {
-			res.send(splitCSV);
+			upload = new UploadData(uploadObject);
+			upload.save().then((savedData)=>{
+				console.log("PG&E Data uploaded:", savedData);
+				res.send("Complete");
+			});
 		});
 	/* ============================== TESTING FILE RESPONSE ============================== */
-
-
 
 });
 
