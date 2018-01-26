f641e57f1f7a71ae9d160d7c02487717101bd18d
Kaylynn <kaylynn.rothleder@gmail.com>
2018-01-25 20:13:52 -0800

    2 charts

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,62 +3,103 @@ import {Bar} from 'react-chartjs-2';
 import API from "../utils/API";
 
 class Dashboard extends Component{
+  constructor(){
+    super();
+    this.state = {
+      KwhChartData:{},
+      CostChartData:{}
+    }
+  }
 
-    state = {
-      chartData:{
-        labels: [1],
-        datasets:[
-          {
-            label:'population',
-            data:[
-              617594,
-              181045,
-              153060
-            ],
-            backgroundColor:[
-              'rgba(255,99,132,0.6)',
-              'rgba(255,99,132,0.6)',
-              'rgba(255,99,132,0.6)'
+  getChartData(userData){
+    //Ajax call
+    API.getUploads()
+      .then(res => {
+        let datasets = [];
+        let dataset = {
+          label:"",
+          backgroundColor: "",
+          data: []
+        }
+        let months = [];
+        let bills = [];
+        let backgroundColor = [
+          'rgba(255,99,132,0.6)',
+          'rgba(255, 0, 0, 0.3)',
+          'rgba(0, 255, 0, 0.3)',
+          'rgba(0, 0, 255, 0.3)',
+          'rgba(255,99,132,0.6)',
+          'rgba(255,99,132,0.6)',
+          'rgba(255,99,132,0.6)',
+          'rgba(255,99,132,0.6)',
+          'rgba(255,99,132,0.6)',
+          'rgba(255,99,132,0.6)',
+          'rgba(255,99,132,0.6)',
+          'rgba(255,99,132,0.6)'
+        ];
+        // loop throuh kwh data
+        for (var i = 1; i < res.data.length; i++) {
+          months.push(res.data[i].kwh);
+        }
+          months.sort()
+        // loop throuh cost data
+        for (var i = 1; i < res.data.length; i++) {
+          bills.push(res.data[i].bill);
+        }
+        console.log(res.data);
+        console.log(months);
+        this.setState({
+          KwhChartData:{
+            labels: months,
+            datasets:[
+              {
+                label:'population',
+                data:bills,
+                backgroundColor:backgroundColor
+              }
+            ]
+          },
+          CostChartData:{
+            labels: months,
+            datasets:[
+              {
+                label:'population',
+                data:bills,
+                backgroundColor:backgroundColor
+              }
             ]
           }
-        ]
-      }
+        })
+
+      })
+      .catch(err => console.log(err));
+  }
+
+
+  componentDidMount(){
+      this.getChartData();
     }
-
-    loadUpload = () => {
-      API.getUploads()
-        .then(res => {
-          let newlabels = this.state.chartData.labels.slice()
-          newlabels.push(100)
-          console.log(newlabels)
-          this.setState({labels:newlabels})
-
-           // this.setState({ 'labels': this.state.chartData.labels.concat([100])});
-           // console.log(this.state.chartData.labels)
-         // this.state.chartData.labels[0]=res.data[1].kwh;
-         //  //console.log(res.data[1].kwh)
-       })
-        .catch(err => console.log(err));
-    };
-
-
-    componentDidMount(){
-      this.loadUpload()
-    }
-
-
-
 
 
   render(){
     return(
       <div className="chart">
         <Bar
-	         data={this.state.chartData}
+	         data={this.state.KwhChartData}
 	         options={{
              title:{
                display: true,
                text:'Engergy Consumption',
+               fontSize: 25
+             }
+           }}
+        />
+        <Bar
+           data={this.state.CostChartData}
+           options={{
+             title:{
+               display: true,
+               text:'Cost',
                fontSize: 25
              }
            }}
