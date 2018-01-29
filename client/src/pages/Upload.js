6736fe28d2c612953011a42ceeec49da52fda1ca
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-29 14:58:09 -0800

    Add Shard alerts and borderColor transition

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -37,12 +37,19 @@ class Upload extends Component {
 
   drop_handler(event) {
     event.preventDefault();
-    console.log(event);
+    console.log(event.type);
   }
 
   dragover_handler(event) {
     event.preventDefault();
-    console.log(event);
+    console.log(event.type);
+    document.getElementById("pgeCsvForm").style.borderColor = "#00b8d8";
+  }
+
+  dragexit_handler(event) {
+    event.preventDefault();
+    console.log(event.type);
+    document.getElementById("pgeCsvForm").style.borderColor = "#e9ecef";
   }
 
   render() {
@@ -50,22 +57,38 @@ class Upload extends Component {
       <Container>
         <div className="row" style={ { "paddingTop": "25px" } }>
           <div className="col-sm-12">
-            <form className="box" action="http://localhost:3001/api/upload" encType="multipart/form-data" method="post">
-              <div className="box__input">
-                <input type="file" className="box__file" onChange={ this.fileInput } onDrop={this.drop_handler} onDragOver={this.drop_handler} name="pgeCsv" id="pgeCsv" />
-                <label htmlFor="file"><strong>Choose a file</strong><span className="box__dragndrop"> or drag it here</span>.</label>
-                <input type="text" style={ { "display": "none" } } name="username" id="username" defaultValue={ this.props.user.local.username } />
-                <input type="text" style={ { "display": "none" } } name="userid" id="userid" defaultValue={ this.props.user._id } />
-              </div>
+            <div className="alert alert-success alert-dismissible fade show" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+              CSV successfully uploaded!
+            </div>
+            <div className="alert alert-danger alert-dismissible fade show" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+              Error uploading CSV!
+            </div>
+            <div className="alert alert-info alert-dismissible fade show" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+              Uploading file!
+            </div>
+          </div>
+          <div className="col-sm-12">
+            <form id="pgeCsvForm" className="box" action="http://localhost:3001/api/upload" encType="multipart/form-data" method="post">
+              <input type="file" className="box__file" onChange={ this.fileInput } onDrop={ this.drop_handler } onDragOver={ this.dragover_handler } onDragLeave={ this.dragexit_handler }
+                name="pgeCsv" id="pgeCsv" />
+              <label htmlFor="file"><strong>Choose a file</strong><span className="box__dragndrop"> or drag it here</span>.</label>
+              <input type="text" style={ { "display": "none" } } name="username" id="username" defaultValue={ this.props.user.local.username } />
+              <input type="text" style={ { "display": "none" } } name="userid" id="userid" defaultValue={ this.props.user._id } />
               <button type="submit" className="box__button">
                 Upload
               </button>
-              <div className="box__uploading">Uploading…</div>
-              <div className="box__success">Done!</div>
-              <div className="box__error">Error! <span></span>.</div>
             </form>
-            {/* <p id="fileData" style={ { paddingTop: "25px", fontSize: "22px" } } />
-            <p id="fileContent" style={ { paddingTop: "25px", fontSize: "22px" } } /> */}
+            <p id="fileData" style={ { paddingTop: "25px", fontSize: "22px" } } />
+            <p id="fileContent" style={ { paddingTop: "25px", fontSize: "22px" } } />
           </div>
         </div>
       </Container>
