2b14d35bf353c127d2c4c939aed08274e5fa25f7
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-30 15:01:09 -0800

    Display messages according to file type

--- client/src/components/Upload/Alert.js
+++ client/src/components/Upload/Alert.js
@@ -3,23 +3,11 @@ import React from "react";
 function Alert(props) {
   return (
     <div className="col-sm-12">
-      <div className="alert alert-success alert-dismissible fade show" role="alert">
-        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">×</span>
-        </button>
-        CSV successfully uploaded!
+      <div id="alert_success" className="alert alert-success alert-dismissible fade show" role="alert">
       </div>
-      <div className="alert alert-danger alert-dismissible fade show" role="alert">
-        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">×</span>
-        </button>
-        Error uploading CSV!
+      <div id="alert_danger" className="alert alert-danger alert-dismissible fade show" role="alert">
       </div>
-      <div className="alert alert-info alert-dismissible fade show" role="alert">
-        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">×</span>
-        </button>
-        Uploading file!
+      <div id="alert_info" className="alert alert-info alert-dismissible fade show" role="alert">
       </div>
     </div>
     );
--- client/src/utils/readcsv.js
+++ client/src/utils/readcsv.js
@@ -3,24 +3,21 @@
 let fileInput = (event) => {
   var file = event.target.files[0] || event.dataTransfer.items[0].getAsFile();
   var textType = /text.csv/;
-  var details = {
-    name: file.name,
-    type: file.type,
-    size: file.size
-  };
 
-  document.getElementById("fileData").innerText = JSON.stringify(details);
+  displayFileData(file);
 
   if (file.type.match(textType)) {
-    var reader = new FileReader();
-
-    reader.onload = function () {
-      document.getElementById("fileContent").innerText = reader.result;
-    };
-
-    reader.readAsText(file);
+    // var reader = new FileReader();
+    // reader.onload = function () {
+    // document.getElementById("fileContent").innerText = reader.result;
+    // };
+    // // reader.readAsText(file);
   } else {
-    document.getElementById("fileContent").innerText = "File not supported!";
+    document.getElementById("alert_danger").innerText = "Unsupported file type! File must be a csv!";
+    document.getElementById("alert_danger").style.display = "block";
+    setTimeout(() => {
+      document.getElementById("alert_danger").style.display = "none";
+    }, 3000);
   }
 };
 
@@ -32,20 +29,35 @@ let choose_handler = (event) => {
 let drop_handler = (event) => {
   event.preventDefault();
   fileInput(event);
+  var form = document.getElementById("pgeCsvForm");
+  form.classList.remove("file__correct","file__incorrect");
 };
 
 let dragover_handler = (event) => {
   event.preventDefault();
   var form = document.getElementById("pgeCsvForm");
-  console.log(event.dataTransfer.items[0].type);
-  form.classList.add("file__correct");
+  var fileType = event.dataTransfer.items[0].type;
+  console.log("File type:", fileType);
+
+  if (fileType === "text/csv") {
+    form.classList.add("file__correct");
+  } else {
+    form.classList.add("file__incorrect");
+  }
 };
 
 let dragexit_handler = (event) => {
   event.preventDefault();
   var form = document.getElementById("pgeCsvForm");
-  form.classList.remove("file__correct");
+  form.classList.remove("file__correct","file__incorrect");
 };
+
+function displayFileData(file) {
+  document.getElementById("fileData").innerHTML =
+  `<div><strong>File name:</strong> ${file.name}\n
+  <div><strong>File type:</strong> ${file.type}\n
+  <div><strong>File type:</strong> ${file.size}\n`;
+}
 
 export {
   choose_handler,
